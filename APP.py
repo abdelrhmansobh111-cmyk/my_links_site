af194a89d32ba7beba1aad49787dbe7d08f7e6bb
Abdelrahman <abdelrhmansobh111@gmail.com>
2026-08-20 13:50:26 +0300

    update cv section

--- APP.py
+++ APP.py
@@ -2,15 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
     links = [
         {"name": "Facebook", "url": "https://www.facebook.com/abdelrhman.sobh.2025"},
         {"name": "Instagram", "url": "https://www.instagram.com/abdelr7man_sobh/?hl=ar"},
         {"name": "Tiktok", "url": "https://www.tiktok.com/@abdelr7man_sobh"},
-        {"name": "WhatsApp", "url": "https://wa.me/201015816031"}, # ضفنا الفاصلة هنا
-        {"name": "github", "url": "https://github.com/abdelrhmansobh111-cmyk"}
+        {"name": "WhatsApp", "url": "https://wa.me/201015816031"},
+        {"name": "GitHub", "url": "https://github.com/abdelrhmansobh111-cmyk"}
     ]
     return render_template("index.html", links=links)
-if __name__ == "__main__":
+
+# المسار الجديد لصفحة الـ CV
+@app.route('/cv')
+def cv():
+    return render_template("cv.html")
+
+if __name__ == '__main__':
     app.run(debug=True)
